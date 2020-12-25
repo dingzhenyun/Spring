c55015154320ff176aa9d88017e9fb1e4711b51d
dingzhenyun <2901082882@qq.com>
2020-12-25 11:12:51 +0800

    入门案例

---
+++ ModuleOfJar/SpringDemo1/src/cn/ecut/dzy/User.java
@@ -0,0 +1,15 @@
+package cn.ecut.dzy;/**
+ * @author DingZhenYun
+ * @create 2020-12-24 下午 16:39
+ */
+
+/**
+ * @program: ModuleOfJar->User
+ * @description: Spring入门
+ **/
+public class User {
+
+    public void test(){
+        System.out.println("Spring5入门案例");
+    }
+}
